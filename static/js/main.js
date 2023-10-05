fdc043199a45ace52ca696aef7872048f22c92a4
ivfarias <imivanfarias@gmail.com>
2023-10-05 13:20:36 -0300

    modified:   static/js/main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1206,7 +1206,7 @@ class App extends StyledComponent {
                     break;
                 case 'welcome':
                     //> This is a predetermined URL that points to the welcome Pigeonpad.
-                    router.go('/f/d6d40c3cd33b/e3b0c44298fc.html/edit', { replace: true });
+                    router.go('https://app.pigeonaut.io/f/d6d40c3cd33b/e3b0c44298fc.html/edit', { replace: true });
                     break;
                 default:
                     {
