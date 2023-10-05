cfcbaed7111a3d5925b55dbdde6005a829d2bf9a
ivfarias <imivanfarias@gmail.com>
2023-10-05 16:58:09 -0300

    modified:   static/js/main.js

--- static/js/main.js
+++ static/js/main.js
@@ -727,8 +727,9 @@ class Editor extends StyledComponent {
     <head>
         <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width,initial-scale=1"/>
-        <link rel="icon" type="image/png" href="/static/assets/favicon-32x32.png" sizes="32x32" />
-        <title>Live Frame | Pigeonpad</title>
+        <link rel="icon" type="image/x-icon" href="/static/assets/favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" type="image/x-icon" href="/static/assets/favicon-16x16.png" sizes="16x16" />
+        <title>Live Pad | Pigeonpad</title>
     </head>
     <body>
         ${this.core.getValue('html')}
